2cde85c2c142fce2bb46f1733df1f15adad8a771
hoang cong minh <0782708546hoangcongminh@gmail.com>
2023-04-17 17:01:11 +0700

    oklogin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4261d650a60e6d89"; }
+/******/ 		__webpack_require__.h = function() { return "130ea5e04c174bf1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Firebase.tsx
+++ Firebase.tsx
@@ -1,27 +1,54 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
 import {getFirestore} from'firebase/firestore'
 import { getAnalytics } from "firebase/analytics";
 import { getApps,getApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/analytics";
 import { EmailAuthProvider, FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCT7k4PVS93yQS_68oaBvGFofttPRFOBGw",
+//     authDomain: "wedphimchill123.firebaseapp.com",
+//     projectId: "wedphimchill123",
+//     storageBucket: "wedphimchill123.appspot.com",
+//     messagingSenderId: "561067670442",
+//     appId: "1:561067670442:web:a1896625356a71b41c89d3",
+//     measurementId: "G-GKF7MK5TP0"
+// };
+
+// // Initialize Firebase
+// const app = getApps().length? getApp(): initializeApp(firebaseConfig)
+// const db= getFirestore(app)
+// const auth=getAuth(app)
+// const provider= new EmailAuthProvider()
+// const providergoogle= new GoogleAuthProvider()
+// const providerfacebook= new FacebookAuthProvider()
+// const providergitup= new GithubAuthProvider()
+
+// // const analytics = getAnalytics(app);
+
+// export default app
+
+// export{db,auth,provider,providergoogle,providerfacebook,providergitup}
+
+
+
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCT7k4PVS93yQS_68oaBvGFofttPRFOBGw",
-    authDomain: "wedphimchill123.firebaseapp.com",
-    projectId: "wedphimchill123",
-    storageBucket: "wedphimchill123.appspot.com",
-    messagingSenderId: "561067670442",
-    appId: "1:561067670442:web:a1896625356a71b41c89d3",
-    measurementId: "G-GKF7MK5TP0"
+  apiKey: "AIzaSyCgPbTFrZMv2J_fb0rNFUki0veHOTkkqs0",
+  authDomain: "apifilebase.firebaseapp.com",
+  projectId: "apifilebase",
+  storageBucket: "apifilebase.appspot.com",
+  messagingSenderId: "650158069250",
+  appId: "1:650158069250:web:854154656992dca8dd391b"
 };
 
-// Initialize Firebase
-const app = getApps().length? getApp(): initializeApp(firebaseConfig)
+ const app = getApps().length? getApp(): initializeApp(firebaseConfig)
 const db= getFirestore(app)
 const auth=getAuth(app)
 const provider= new EmailAuthProvider()
